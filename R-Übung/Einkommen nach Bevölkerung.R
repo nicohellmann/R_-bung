56b49f14bef356a3617b5b713a96d72bea253e45
unknown <hellmann1.nico@gmx.de>
2024-06-18 14:15:54 +0200

    finals

--- R-Übung/Einkommen nach Bevölkerung.R
+++ R-Übung/Einkommen nach Bevölkerung.R
@@ -1,1 +1,11 @@
-anime
+library(readxl)
+Einkommen <- read_xlsx("R_-bung/R-Übung/Einkommen.xlsx",sheet = 1)
+View(Einkommen$`Bevölkerung in Hauptwohnsitzhaushalten`)
+View(Einkommen)
+vector_BIH <- Einkommen$`Bevölkerung in Hauptwohnsitzhaushalten`[3:41]
+vector_BIH <- vector_BIH[-c(13,26,39)]
+BIH <- median(vector_BIH)
+modus_BIH <- sort(vector_BIH)[1]
+vector_BIH_männlich <- vector_BIH[4:12]
+vector_BIH_weiblich <- vector_BIH[17:28]
+boxplot(vector_BIH_männlich,vector_BIH_weiblich,axes = TRUE,ylab = "Einkommen",names=c("Männlich","Weiblich"),staplewex = 1)
